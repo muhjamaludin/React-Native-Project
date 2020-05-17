caf853918b580239388c214a7689f10ad2ea3351
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-05-17 11:17:06 +0700

    move component and helpers to src

--- MemoryGame/App.js
+++ MemoryGame/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {Ionicons, FontAwesome, Entypo} from '@expo/vector-icons'
+
+import Header from './src/component/Header'
+import Score from './src/component/Score'
+import Card from './src/component/Card'
+import Helper from './src/helpers/helpers'
 
 export default function App() {
   return (
